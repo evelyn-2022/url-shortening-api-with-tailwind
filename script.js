1c316ecb741a0b7a85c81f4654968119a6964c0b
evelyn-2022 <evelyn20220101@gmail.com>
2022-07-02 12:25:26 +0800

    finished shorten links part

---
+++ script.js
@@ -0,0 +1,128 @@
+// Mobile menu
+const menuContainer = document.querySelector('.menu-container');
+const menuIcon = document.querySelector('.menu-icon');
+
+menuIcon.addEventListener('click', () => {
+  menuContainer.classList.toggle('open');
+});
+
+///////////////////////////////////////////////////////////////////
+// Shorten link
+const inputCon = document.querySelector('.input');
+const shortenBtn = document.querySelector('.input button');
+const userInp = document.querySelector('.input input');
+const warning = document.querySelector('.warning');
+let results = [];
+class Result {
+  constructor(id, orig, shortened) {
+    this.id = id;
+    this.orig = orig;
+    this.shortened = shortened;
+  }
+}
+
+getLocalStorage();
+
+shortenBtn.addEventListener('click', () => {
+  const inp = userInp.value;
+
+  // Check for empty input
+  if (inp.length === 0) {
+    userInp.classList.add('invalid');
+    warning.classList.remove('hidden');
+    return;
+  } else {
+    userInp.classList.remove('invalid');
+    warning.classList.add('hidden');
+  }
+
+  // Ajax call
+  (async function (link) {
+    try {
+      const data = await getShortenedLink(link);
+    } catch (err) {
+      alert(err);
+    }
+  })(inp);
+});
+
+async function getShortenedLink(link) {
+  try {
+    const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${link}`);
+    if (!res.ok) throw new Error('Please enter a valid link');
+
+    const data = await res.json();
+    const { full_short_link: shortLink, original_link: origLink } = data.result;
+    const id = shortLink.slice(-6);
+    renderResult(id, origLink, shortLink);
+    addToList(id, origLink, shortLink);
+    setLocalStorage();
+  } catch (err) {
+    throw err;
+  }
+}
+
+function renderResult(id, orig, shortened) {
+  inputCon.insertAdjacentHTML(
+    'afterend',
+    `     <div
+            class="result flex flex-col bg-white rounded-lg md:items-center md:flex-row md:justify-between md:py-4 md:px-6 md:text-xl md:space-x-6"
+          >
+            <!-- Original Link -->
+            <div
+              class="text-veryDarkViolet p-4 pb-3 border-b-[1px] border-b-gray/25 truncate md:border-none md:p-0"
+            >
+              ${orig}
+            </div>
+
+            <!-- Shortened Link -->
+            <div
+              class="flex flex-col px-4 md:flex-row md:items-center md:p-0 md:justify-end"
+            >
+              <a href="https://shrtco.de/dZKX6e" class="py-3 text-cyan"
+                >${shortened}</a
+              >
+              <button
+                id="${id}"
+                onclick="copyText('${id}')"
+                class="copy-btn bg-cyan text-white py-2.5 px-2 mb-4 rounded-md md:mb-0 md:text-lg md:w-32 md:ml-6"
+              >
+                Copy
+              </button>
+            </div>
+          </div>`
+  );
+}
+
+function addToList(id, orig, short) {
+  const newRes = new Result(id, orig, short);
+  results.push(newRes);
+}
+
+function setLocalStorage() {
+  localStorage.setItem('results', JSON.stringify(results));
+}
+
+function getLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('results'));
+
+  if (!data) return;
+
+  results = data;
+  results.forEach(result => {
+    renderResult(result.id, result.orig, result.shortened);
+  });
+}
+
+function reset() {
+  localStorage.removeItem('results');
+  // location.reload();
+}
+
+function copyText(id) {
+  const btn = document.getElementById(id);
+  btn.textContent = 'Copied!';
+  btn.style.backgroundColor = 'hsl(257, 27%, 26%)';
+  const copyText = `https://shrtco.de/` + id;
+  navigator.clipboard.writeText(copyText);
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      cyan: 'hsl(180, 66%, 49%)',
+      darkViolet: 'hsl(257, 27%, 26%)',
+      red: 'hsl(0, 87%, 67%)',
+      gray: 'hsl(0, 0%, 75%)',
+      grayishViolet: 'hsl(257, 7%, 63%)',
+      veryDarkBlue: 'hsl(255, 11%, 22%)',
+      veryDarkViolet: 'hsl(260, 8%, 14%)',
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
